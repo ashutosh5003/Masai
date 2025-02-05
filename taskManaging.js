7cd015a741597454d1b3be0b90461628c7732c78
Ashutosh <ashutoshgaur50@gmail.com>
2025-02-05 09:51:36 +0530

    add taskManaging.js to manage daily tasks

---
+++ taskManaging.js
@@ -0,0 +1,6 @@
+let task = ["wakeup", "brush", "breakfast", "office", "Lunch", "office", "dinner", "sleep"];    
+task.shift();
+task.unshift("dance", "sing");  
+task.pop();
+task.push("gym");
+console.log(task)
