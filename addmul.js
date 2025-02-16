a0a831747d9550f14ac32a1cc501854a8269fc3b
Ashutosh <ashutoshgaur50@gmail.com>
2025-02-16 21:13:48 +0530

    add addmul.js to calculate square and cube of a number; fix variable name in covnertToArrow.js

---
+++ addmul.js
@@ -0,0 +1,5 @@
+const solve = (num1) => {
+  let sq = num1*num1;
+  let cube = num11*num1*num1;
+  return {sq, cube};
+}
--- covnertToArrow.js
+++ covnertToArrow.js
@@ -2,6 +2,6 @@ function multiply(a , b ) {
     return a * b; 
 }
 
-function convertToArrow = (a, b) => {
+const convertToArrow = (a, b) => {
     return a * b;
 }
